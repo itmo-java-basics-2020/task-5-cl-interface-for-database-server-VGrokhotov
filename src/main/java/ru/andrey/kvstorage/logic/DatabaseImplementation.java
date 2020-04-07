ad5e2f3530cfd265bf48c654af34957026f4ee8e
Vladislav <vlad@grokhotov.ru>
2020-04-07 23:58:36 +0300

    added Database Implementation

---
+++ src/main/java/ru/andrey/kvstorage/logic/DatabaseImplementation.java
@@ -0,0 +1,62 @@
+package ru.andrey.kvstorage.logic;
+
+import ru.andrey.kvstorage.exception.DatabaseException;
+
+import java.util.HashMap;
+
+public final class DatabaseImplementation implements Database {
+
+    private final HashMap<String, HashMap<String, String>> tables;
+    private final String name;
+
+    public DatabaseImplementation(String name) {
+        this.tables = new HashMap<>();
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void createTableIfNotExists(String tableName) throws DatabaseException {
+
+        if ( this.tables.containsKey(tableName) ){
+            throw new DatabaseException( "Table " + tableName + " already exists");
+        }
+
+        this.tables.put(tableName, new HashMap<>());
+
+    }
+
+    @Override
+    public void createTableIfNotExists(String tableName, int segmentSizeInBytes) throws DatabaseException {
+        //
+    }
+
+    @Override
+    public void write(String tableName, String objectKey, String objectValue) throws DatabaseException {
+
+        if ( this.tables.containsKey(tableName) ){
+            this.tables.get(tableName).put(objectKey, objectValue);
+        } else {
+            //throw new DatabaseException("There is no key " + objectKey + " in table " + tableName);
+        }
+
+    }
+
+    @Override
+    public String read(String tableName, String objectKey) throws DatabaseException {
+
+        if ( this.tables.containsKey(tableName) ){
+            return this.tables.get(tableName).getOrDefault(objectKey, null);
+            //throw new DatabaseException("no table" + tableName);
+        } else {
+            //throw new DatabaseException("There is no key " + objectKey + " in table " + tableName);
+            return null;
+        }
+
+    }
+
+}
