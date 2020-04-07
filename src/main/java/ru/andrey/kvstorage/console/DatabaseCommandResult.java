0125c4e6a4b9756ea861c52998a904e6bcaad71e
Vladislav <vlad@grokhotov.ru>
2020-04-08 00:54:54 +0300

    added DatabaseCommandResult implementation

--- src/main/java/ru/andrey/kvstorage/console/DatabaseCommandResult.java
+++ src/main/java/ru/andrey/kvstorage/console/DatabaseCommandResult.java
@@ -1,5 +1,6 @@
 package ru.andrey.kvstorage.console;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public interface DatabaseCommandResult {
@@ -15,4 +16,55 @@ public interface DatabaseCommandResult {
     enum DatabaseCommandStatus {
         SUCCESS, FAILED
     }
+
+    static DatabaseCommandResult success(String result){
+        return new DatabaseCommandResultImplementation(result, null, DatabaseCommandStatus.SUCCESS);
+    }
+
+    static DatabaseCommandResult error(String message){
+        return new DatabaseCommandResultImplementation(null, message, DatabaseCommandStatus.FAILED);
+    }
+
+    final class DatabaseCommandResultImplementation implements DatabaseCommandResult {
+
+        private final String result;
+        private final String errorMessage;
+        private final DatabaseCommandStatus status;
+
+        public DatabaseCommandResultImplementation(String result, String errorMessage, DatabaseCommandStatus status) {
+            this.result = result;
+            this.errorMessage = errorMessage;
+            this.status = status;
+        }
+
+        @Override
+        public Optional<String> getResult() {
+            return Optional.ofNullable(result);
+        }
+
+        @Override
+        public DatabaseCommandStatus getStatus() {
+            return status;
+        }
+
+        @Override
+        public boolean isSuccess() {
+            return status.equals(DatabaseCommandStatus.SUCCESS);
+        }
+
+        @Override
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+
+        @Override
+        public String toString() {
+            return "DatabaseCommandResultImplementation:" +
+                    "result = \"" + result + '\"' +
+                    ", errorMessage = \"" + errorMessage + '\"' +
+                    ", status = " + status;
+        }
+    }
+
 }
+
